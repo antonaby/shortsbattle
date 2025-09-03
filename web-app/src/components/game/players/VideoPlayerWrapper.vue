8549258039df0553e0398c0d83e34a2f4b0ffb87
Anton Abyshev <abyshev.anton@gmail.com>
2025-09-03 16:32:07 +0400

    add final screen

--- web-app/src/components/game/players/VideoPlayerWrapper.vue
+++ web-app/src/components/game/players/VideoPlayerWrapper.vue
@@ -46,14 +46,14 @@ function nextVideo() {
   if (currentVideoIndex.value < props.videoUrls.length - 1) {
     currentVideoIndex.value += 1;
   } else {
-    currentVideoIndex.value = 0;
+    currentVideoIndex.value = -1;
   }
 }
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center bg-gray-200 w-screen h-screen space-y-2">
-    <div 
+    <div v-if="currentVideoIndex >= 0"
       class="transition-[width] duration-500 ease-in-out" 
       :class="[showMenu ? 'w-7/8' : 'w-full']">
       <YTPlayer v-if="platform == 'youtube'"
@@ -65,28 +65,36 @@ function nextVideo() {
         @state-change="stateChangeTT" 
         @error="handleErrorTT" />
     </div>
-    <Transition 
+    <Transition v-if="currentVideoIndex >= 0"
       enter-active-class="transition duration-300 ease-out" 
       enter-from-class="opacity-0 translate-y-2"
       enter-to-class="opacity-100 translate-y-0" 
       leave-active-class="transition duration-200 ease-in"
       leave-from-class="opacity-100 translate-y-0" 
       leave-to-class="opacity-0 translate-y-2">
-      <div v-if="showMenu" class="bg-white rounded-xl p-2 flex justify-center items-center space-x-2">
-        <button class="w-16 rounded-md  p-2 font-medium" @click="pinMenu = !pinMenu">
+      <div v-if="showMenu" class="bg-white rounded-xl p-2 flex justify-center items-center space-x-2 shadow">
+        <button class="w-16 rounded-md p-2 font-medium" @click="pinMenu = !pinMenu">
           <span v-if="!pinMenu">📌</span>
           <span v-else>📍</span>
         </button>
-        <button class="w-16 rounded-md  p-2 font-medium" @click="nextVideo">
+        <button class="w-16 rounded-md p-2" @click="nextVideo">
           <span>🙈</span>
         </button>
-        <button class="w-16 rounded-md  p-2 font-medium" @click="nextVideo">
+        <button class="w-16 rounded-md p-2" @click="nextVideo">
           <span>👍</span>
         </button>
-        <button class="w-16 rounded-md  p-2 font-medium" @click="nextVideo">
+        <button class="w-16 rounded-md p-2" @click="nextVideo">
           <span>👎</span>
         </button>
       </div>
     </Transition>
+    <div v-else class="bg-white rounded-xl p-4 shadow flex flex-col items-center">
+      <span class="text-2xl font-medium">
+        You have watched all videos 🥳
+      </span>
+      <button class="w-16 rounded-md p-2 text-2xl">
+        <span>👍</span>
+      </button>
+    </div>
   </div>
 </template>
