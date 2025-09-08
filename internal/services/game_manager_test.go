bc90d24f6dd1f6913a7cc80feb151cd361c33a89
Anton Abyshev <abyshev.anton@gmail.com>
2025-09-08 15:04:43 +0400

    use testify

--- internal/services/game_manager_test.go
+++ internal/services/game_manager_test.go
@@ -8,12 +8,20 @@ import (
 	"github.com/antonaby/shortsbattle/game-server/internal/db/qg"
 	"github.com/antonaby/shortsbattle/game-server/internal/tests"
 	"github.com/jackc/pgx/v5"
+	"github.com/ory/dockertest/v3"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestAdvanceGame(t *testing.T) {
+type GameManagerTestSuite struct {
+	pool        *dockertest.Pool
+	pgContainer *dockertest.Resource
+	dbManager   *db.DbManager
+}
+
+func NewGMTestSuite(t *testing.T) *GameManagerTestSuite {
 	pool, err := tests.CreateDockerPool()
 	if err != nil {
-		t.Fatalf("could not connect to docker: %v", err)
+		t.Fatalf("failed to connect to docker: %v", err)
 	}
 
 	pgUser := "admin"
@@ -22,7 +30,7 @@ func TestAdvanceGame(t *testing.T) {
 
 	pgContainer, err := tests.CreatePostgresContainer(t, pool, pgUser, pgPassword, pgDb)
 	if err != nil {
-		t.Fatalf("could not create Postgres container: %v", err)
+		t.Fatalf("failed to create pg container: %v", err)
 	}
 
 	dbManager, err := tests.CreateDbManager(pool, pgContainer, pgUser, pgPassword, pgDb)
@@ -30,32 +38,44 @@ func TestAdvanceGame(t *testing.T) {
 		t.Fatalf("failed to create db manager: %v", err)
 	}
 
-	defer dbManager.Close()
-
-	err = db.WithTx(context.Background(), dbManager, func(ctx context.Context, tx pgx.Tx) error {
-		q := dbManager.Querier(tx)
-		_, err := q.CreatePlayer(ctx, qg.CreatePlayerParams{
-			TgID: 1,
-		})
-
-		return err
+	t.Cleanup(func() {
+		dbManager.Close()
 	})
 
-	if err != nil {
-		t.Fatalf("failed to create user")
+	return &GameManagerTestSuite{
+		pool:        pool,
+		pgContainer: pgContainer,
+		dbManager:   dbManager,
 	}
+}
 
-	player, err := db.WithTxVQ(context.Background(), dbManager, func(ctx context.Context, q qg.Querier) (qg.Player, error) {
-		return q.GetPlayerByTgId(ctx, qg.GetPlayerByTgIdParams{
-			TgID: 1,
+func TestAdvanceGame(t *testing.T) {
+	ts := NewGMTestSuite(t)
+
+	t.Run("HandleLobby", func(t *testing.T) {
+		err := db.WithTx(context.Background(), ts.dbManager, func(ctx context.Context, tx pgx.Tx) error {
+			q := ts.dbManager.Querier(tx)
+			_, err := q.CreatePlayer(ctx, qg.CreatePlayerParams{
+				TgID: 1,
+			})
+
+			return err
 		})
+
+		if err != nil {
+			t.Fatalf("failed to create user")
+		}
+
+		player, err := db.WithTxVQ(context.Background(), ts.dbManager, func(ctx context.Context, q qg.Querier) (qg.Player, error) {
+			return q.GetPlayerByTgId(ctx, qg.GetPlayerByTgIdParams{
+				TgID: 1,
+			})
+		})
+
+		if err != nil {
+			t.Fatalf("failed to create player")
+		}
+
+		assert.Equal(t, int64(1), player.TgID)
 	})
-
-	if err != nil {
-		t.Fatalf("failed to create fetch")
-	}
-
-	if player.TgID != 1 {
-		t.Errorf("incorrect tgId")
-	}
 }
