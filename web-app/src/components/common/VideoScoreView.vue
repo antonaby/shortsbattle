148c4e206076478d68c21b97794e993533f2aa6c
Anton Abyshev <abyshev.anton@gmail.com>
2025-09-24 10:51:29 +0400

    move likes/dislikes on the left side

--- web-app/src/components/common/VideoScoreView.vue
+++ web-app/src/components/common/VideoScoreView.vue
@@ -20,47 +20,44 @@ const videoFrom = computed<string>(() => {
 
 <template>
   <div class="flex flex-wrap gap-2 items-start">
-    <div v-if="videoResult.video.oembed.thumbnail_url" 
-      class="thumbnail-container">
-      <img 
-        :src="videoResult.video.oembed.thumbnail_url" 
-        :alt="videoResult.video.oembed.title || 'Video Cover'" 
-        class="object-cover max-h-28" 
-        loading="lazy" />
+    <div class="flex flex-wrap flex-col gap-1 w-10">
+      <div class="ld-container border-emerald-300 text-emerald-700">
+        <p>👍</p>
+        <p>{{ videoResult.likes }}</p>
+      </div>
+      <div class="ld-container border-rose-300 text-rose-700">
+        <p>👎</p>
+        <p>{{ videoResult.dislikes }}</p>
+      </div>
+    </div>
+    <div v-if="videoResult.video.oembed.thumbnail_url" class="thumbnail-container">
+      <img :src="videoResult.video.oembed.thumbnail_url" :alt="videoResult.video.oembed.title || 'Video Cover'"
+        class="object-cover max-h-28" loading="lazy" />
     </div>
     <div v-else class="thumbnail-container min-h-26 text-gray-600 font-light">
       <span>No Image</span>
     </div>
-    <div class="flex-1 flex flex-col gap-2">
+    <div class="flex-1 flex flex-col gap-1">
       <div class="flex flex-wrap items-center justify-start gap-1">
         <div class="text-sm px-2 py-1 rounded-lg bg-gradient-to-r from-cyan-500 to-blue-600 text-white">
           <span>👤</span>
           <span class="font-semibold max-w-[120px] truncate">{{ videoResult.author.username }}</span>
         </div>
-        <VHIcon :platform="platform" :author="videoFrom" />
       </div>
+      <VHIcon :platform="platform" :author="videoFrom" />
       <span class="text-sm">{{ videoResult.video.oembed.title || "no title" }}</span>
-      <div class="flex flex-wrap gap-2">
-        <div class="ld-container border-emerald-300 text-emerald-700">
-          <span>👍</span>
-          <span>{{ videoResult.likes }}</span>
-        </div>
-        <div class="ld-container border-rose-300 text-rose-700">
-          <span>👎</span>
-          <span>{{ videoResult.dislikes }}</span>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <style>
 @import "tailwindcss";
+
 .thumbnail-container {
   @apply flex justify-center items-center bg-gray-200 rounded-lg overflow-hidden w-1/3 min-w-28;
 }
 
 .ld-container {
-  @apply flex-1 text-sm px-2 py-1 rounded-lg text-center font-medium border;
+  @apply flex-1 text-sm px-1 rounded-lg text-center font-medium border;
 }
 </style>
