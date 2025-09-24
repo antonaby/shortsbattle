563f61dcf64f65c23ee9bcfe3d95936087bba53f
Anton Abyshev <abyshev.anton@gmail.com>
2025-09-24 10:43:18 +0400

    fix image url

--- web-app/src/components/common/VideoScoreView.vue
+++ web-app/src/components/common/VideoScoreView.vue
@@ -20,9 +20,16 @@ const videoFrom = computed<string>(() => {
 
 <template>
   <div class="flex flex-wrap gap-2 items-start">
-    <div class="flex justify-center items-center bg-gray-100 rounded-lg overflow-hidden w-1/3 min-w-28">
-      <img src="https://i.ytimg.com/vi/fM9Xt04ieIM/hq2.jpg" alt="Video Info" class="object-cover max-h-28"
+    <div v-if="videoResult.video.oembed.thumbnail_url" 
+      class="thumbnail-container">
+      <img 
+        :src="videoResult.video.oembed.thumbnail_url" 
+        :alt="videoResult.video.oembed.title || 'Video Cover'" 
+        class="object-cover max-h-28" 
         loading="lazy" />
+    </div>
+    <div v-else class="thumbnail-container min-h-26 text-gray-600 font-light">
+      <span>No Image</span>
     </div>
     <div class="flex-1 flex flex-col gap-2">
       <div class="flex flex-wrap items-center justify-start gap-1">
@@ -49,6 +56,9 @@ const videoFrom = computed<string>(() => {
 
 <style>
 @import "tailwindcss";
+.thumbnail-container {
+  @apply flex justify-center items-center bg-gray-200 rounded-lg overflow-hidden w-1/3 min-w-28;
+}
 
 .ld-container {
   @apply flex-1 text-sm px-2 py-1 rounded-lg text-center font-medium border;
--- web-app/src/components/game/subpages/CompleteView.vue
+++ web-app/src/components/game/subpages/CompleteView.vue
@@ -104,107 +104,12 @@ const rounds = computed<RoundResult[]>(() => {
     </button>
     <div class="sub-container" v-if="rounds.length > 0">
       <TabView :tabs="tabs" :selected-key="activeTab.key" @select="onTabSelect" />
-
       <template v-for="round in rounds" :key="round.round.round_n">
         <RoundView :round="round.round" />
         <template v-for="video in round.videos">
           <VideoScoreView :videoResult="video" />
         </template>
       </template>
-
-
-
-
-
-
-
-      <div class="flex items-center gap-4 bg-gray-100 px-2 py-2 rounded-lg my-2">
-        <img src="https://cdn2.thecatapi.com/images/bpc.jpg" alt="Round" class="w-16 h-16 object-cover rounded-lg" />
-        <div class="flex-1">
-          <h2 class="text-title-item">1. Curious Cats</h2>
-          <p class="text-description">Capture cats playing with toys, chasing, or just being mischievous.</p>
-        </div>
-      </div>
-      <!-- Video Info -->
-      <!-- Video Info -->
-      <div class="flex gap-3 w-full items-start">
-        <div class="flex justify-center items-center bg-gray-100 rounded-xl overflow-hidden w-1/3">
-          <img src="https://i.ytimg.com/vi/fM9Xt04ieIM/hq2.jpg" alt="Video Info" class="object-cover max-h-28"
-            loading="lazy" />
-        </div>
-        <div class="flex-1 flex flex-col items-start gap-1">
-          <div class="flex items-center gap-2 text-sm px-2 py-1 rounded-lg bg-gradient-to-r from-cyan-500 to-blue-600
-text-white">
-            <span>👤</span>
-            <span class="font-semibold max-w-[120px] truncate">PlayerTwo</span>
-          </div>
-          <div class="flex justify-start items-center gap-2">
-            <VHIcon platform="youtube" author="ХЛЕБЕЦ"/>
-          </div>
-          <span class="text-sm">that is not his first time #memes #funny #shorts"</span>
-          <div class="w-full flex gap-2">
-            <span
-              class="flex-1 p-1 rounded-lg text-center font-medium border border-green-300 text-green-600 text-sm">👍
-              10</span>
-            <span class="flex-1 p-1 rounded-lg text-center font-medium border border-red-300 text-red-600 text-sm">👎
-              5</span>
-          </div>
-        </div>
-      </div>
-      <!-- Video Info -->
-      <div class="flex gap-3 w-full items-start">
-        <div class="flex justify-center items-center bg-gray-100 rounded-xl overflow-hidden w-1/3">
-          <img src="https://i.ytimg.com/vi/fM9Xt04ieIM/hq2.jpg" alt="Video Info" class="object-cover max-h-28"
-            loading="lazy" />
-        </div>
-        <div class="flex-1 flex flex-col items-start gap-1">
-          <div class="flex items-center gap-2 text-sm px-2 py-1 rounded-lg bg-gradient-to-r from-orange-400 via-pink-500 to-purple-600
-text-white">
-            <span>👤</span>
-            <span class="font-semibold max-w-[120px] truncate">Yours</span>
-          </div>
-          <div class="flex justify-start items-center gap-2">
-            <VHIcon platform="youtube" author="TTTTTTT" />
-          </div>
-          <span class="text-sm">that is not his first time #memes #funny #shorts"</span>
-          <div class="w-full flex gap-2">
-            <span
-              class="flex-1 p-1 rounded-lg text-center font-medium border border-green-300 text-green-600 text-sm">👍
-              10</span>
-            <span class="flex-1 p-1 rounded-lg text-center font-medium border border-red-300 text-red-600 text-sm">👎
-              5</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="sub-container">
-      <div class="flex items-center gap-4 bg-gray-100 px-2 py-2 rounded-lg my-2">
-        <img src="https://cdn2.thecatapi.com/images/bpc.jpg" alt="Round" class="w-16 h-16 object-cover rounded-lg" />
-        <div class="flex-1">
-          <h2 class="text-title-item">2. Sleepy Whiskers</h2>
-          <p class="text-description">Capture cats playing with toys, chasing, or just being mischievous.</p>
-        </div>
-      </div>
-      <!-- Video Info -->
-      <div class="flex gap-4 w-full items-start">
-        <div class="flex justify-center items-center bg-gray-100 rounded-xl overflow-hidden w-1/3">
-          <img src="https://i.ytimg.com/vi/fM9Xt04ieIM/hq2.jpg" alt="Video Info" class="object-cover max-h-28"
-            loading="lazy" />
-        </div>
-        <div class="flex-1 flex flex-col items-start">
-          <div class="flex justify-start items-center gap-2">
-            <VHIcon platform="youtube" author="ХЛЕБЕЦ" />
-          </div>
-          <span class="text-sm">that is not his first time #memes #funny #shorts"</span>
-          <div class="w-full flex gap-2 mt-2">
-            <span
-              class="flex-1 p-1 rounded-lg text-center font-medium border border-green-300 text-green-600 text-sm">👍
-              10</span>
-            <span class="flex-1 p-1 rounded-lg text-center font-medium border border-red-300 text-red-600 text-sm">👎
-              5</span>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
